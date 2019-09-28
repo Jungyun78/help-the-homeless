79df927a89a4cdb8df595f8a6ecba817a29bac00
Rashi Dembi <rashi@wirelessprv-10-195-245-122.near.illinois.edu>
2019-09-28 12:09:48 -0500

    Base website

---
+++ flask/app.py
@@ -0,0 +1,52 @@
+from flask import Flask
+from flask import render_template
+
+# creates a Flask application, named app
+app = Flask(__name__)
+
+# a route where we will display a welcome message via an HTML template
+@app.route("/")
+def hello():
+    message = "Hello, World"
+    return render_template('index.html', message=message)
+
+@app.route("/home")
+def home():
+    message = "Hello, World"
+    return render_template('index.html', message=message)
+
+@app.route("/about-us")
+def about():
+    message = "Hello, World"
+    return render_template('about.html', message=message)
+
+@app.route("/what-do-we-do")
+def what():
+    message = "Hello, World"
+    return render_template('what.html', message=message)
+
+@app.route("/datasets")
+def datasets():
+    message = "Hello, World"
+    return render_template('datasets.html', message=message)
+
+@app.route("/get-involved")
+def involved():
+    message = "Hello, World"
+    return render_template('donate.html', message=message)
+
+@app.route("/what-do-we-do")
+def donate():
+    message = "Hello, World"
+    return render_template('donate.html', message=message)
+
+@app.route("/volunteer")
+def volunteer():
+    message = "Hello, World"
+    return render_template('volunteer.html', message=message)
+
+
+
+# run the application
+if __name__ == "__main__":
+    app.run(debug=True)
